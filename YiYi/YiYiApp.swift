4287b17a39c5e8386ebb279a592a66233f9000b5
John Melody Me <johnmelodyme@icloud.com>
2023-02-24 14:39:27 +0800

    Initial Commit

---
+++ YiYi/YiYiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YiYiApp.swift
+//  YiYi
+//
+//  Created by John Melody Me on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct YiYiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
